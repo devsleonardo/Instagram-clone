6e89080c4e9db54d11a854d54ed78b003862621b
devsleonardo <usr.leonardo@gmail.com>
2021-12-29 16:39:30 -0300

    4.0

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,21 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter();
+
+  public form: FormGroup = new FormGroup({
+    verificacao: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
 
   public exibirPainelCadastro(): void {
     this.exibirPainel.emit('cadastro');
+  }
+
+  public loginUsuario(): void {
+    console.log(this.form.value);
   }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,10 +9,38 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class RegisterComponent implements OnInit {
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter();
 
+  public form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    nameFull: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(120),
+    ]),
+    nameUser: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
+    numberPhone: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(20),
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+    ]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
   public exibirPainelLogin(): void {
     this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario(): void {
+    console.log(this.form.value);
   }
 }
